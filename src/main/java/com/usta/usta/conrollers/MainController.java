0ba2105e082f256f6c4db05bb83e5bcf7333d39e
unknown <german.komarov@ufaz.az>
2020-07-17 16:46:05 +0400

    second commit

--- src/main/java/com/usta/usta/conrollers/MainController.java
+++ src/main/java/com/usta/usta/conrollers/MainController.java
@@ -24,26 +24,13 @@ public class MainController {
 
 
     @GetMapping
-    public String mainPage(@RequestParam(defaultValue = "multiple",name = "category") String category, @PageableDefault (size=20,sort={"id"},direction = Sort.Direction.ASC) Pageable pageable, Model model)
+    @ResponseBody
+    public Page<Person> mainPage(@RequestParam(defaultValue = "multiple",name = "category") String category, @PageableDefault (size=20,sort={"id"},direction = Sort.Direction.ASC) Pageable pageable, Model model)
     {
         Page<Person> people=personService.getPeopleByCategory(category,pageable);
         model.addAttribute("num",people.getTotalElements());
-        return "main";
-
+        return people;
     }
 
-    @GetMapping("/fill")
-    @ResponseBody
-    public String fghj()
-    {
-        for(int i=0;i<100;i++)
-        {
-            Person person=new Person();
-            person.setCategory("multiple");
-            person.setName("German" +i);
-            person.setRoles(Collections.singleton(new Role(1L,"ROLE_USER")));
-            personService.savePerson(person);
-        }
-        return "OK";
-    }
+
 }
